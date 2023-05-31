da825990dfdade3e21ecf4ab0bdf90b5f8f1208f
Rati Rastogi <ratirastogi@Ratis-MacBook-Pro.local>
2023-05-31 12:43:27 -0700

    adding import button

---
+++ modules/ui/tools/import.js
@@ -0,0 +1,91 @@
+import { interpolateRgb as d3_interpolateRgb } from 'd3-interpolate';
+
+import { t } from '../../core/localizer';
+import { svgIcon } from '../../svg';
+import { uiCmd } from '../cmd';
+import { uiTooltip } from '../tooltip';
+import { actionAddEntity } from '../../actions';
+import { osmNode } from '../../osm';
+
+
+export function uiToolImport(context) {
+
+    var tool = {
+        id: 'import',
+        label: t.append('import.title')
+    };
+
+    var button = null;
+    var tooltipBehavior = null;
+    var key = uiCmd('⌘I');
+
+    tool.render = function(selection) {
+        tooltipBehavior = uiTooltip()
+            .placement('bottom')
+            .title(() => t.append('import.title'))
+            .keys([key])
+            .scrollContainer(context.container().select('.top-toolbar'));
+
+        var lastPointerUpType;
+
+        button = selection
+            .append('button')
+            .attr('class', 'save bar-button')
+            .on('pointerup', function(d3_event) {
+                lastPointerUpType = d3_event.pointerType;
+            })
+            .on('click', function(d3_event) {
+                const input = document.createElement('input');
+                input.type = 'file';
+                input.accept = '.geojson'; // Accept GeoJSON files
+                input.onchange = handleFileSelection;
+                input.click();
+
+                function handleFileSelection(event) {
+                    const file = event.target.files[0];
+                    if (file) {
+                      // Read the GeoJSON file
+                      const reader = new FileReader();
+                      reader.onload = function(e) {
+                        const contents = e.target.result;
+                        const geojson = JSON.parse(contents);
+                        if (geojson.type === 'FeatureCollection') {
+                          // Extract points from the GeoJSON and display them on the map
+                          const points = geojson.features.filter(
+                            feature => feature.geometry.type === 'Point'
+                          );
+                          displayPointsOnMap(points);
+                        } else {
+                          console.log('Invalid GeoJSON file. Expected FeatureCollection.');
+                        }
+                      };
+                      reader.readAsText(file);
+                    }
+                  }
+
+                  function displayPointsOnMap(points) {
+              
+                    // Add imported points to the map
+                    points.forEach(function(point) {
+                      const nodeId = point.id;
+                      const loc = [point.geometry.coordinates[0], point.geometry.coordinates[1]];
+                     const node = osmNode({ id: nodeId, loc: loc });
+                      context.perform(actionAddEntity(node), 'add imported point');
+                      console.log('Points added successfully');
+                    });
+                  }
+
+            })
+            .call(tooltipBehavior);
+
+        button
+            .call(svgIcon('#iD-icon-load'));
+
+        button
+            .append('span')
+            .attr('class', 'count')
+            .attr('aria-hidden', 'true')
+            .text('0');
+    };
+    return tool;
+}
--- modules/ui/top_toolbar.js
+++ modules/ui/top_toolbar.js
@@ -3,8 +3,7 @@ import {
 } from 'd3-selection';
 
 import _debounce from 'lodash-es/debounce';
-import { uiToolDrawModes, uiToolNotes, uiToolSave, uiToolSidebarToggle, uiToolUndoRedo } from './tools';
-
+import { uiToolDrawModes, uiToolNotes, uiToolSave, uiToolSidebarToggle, uiToolUndoRedo, uiToolImport } from './tools';
 
 export function uiTopToolbar(context) {
 
@@ -13,6 +12,7 @@ export function uiTopToolbar(context) {
         notes = uiToolNotes(context),
         undoRedo = uiToolUndoRedo(context),
         save = uiToolSave(context);
+        importTool = uiToolImport(context);
 
     function notesEnabled() {
         var noteLayer = context.layers().layer('notes');
@@ -49,7 +49,7 @@ export function uiTopToolbar(context) {
                 tools = tools.concat([notes, 'spacer']);
             }
 
-            tools = tools.concat([undoRedo, save]);
+            tools = tools.concat([undoRedo, save, importTool]);
 
             var toolbarItems = bar.selectAll('.toolbar-item')
                 .data(tools, function(d) {
